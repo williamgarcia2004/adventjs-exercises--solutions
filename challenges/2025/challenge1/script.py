14743a0bd2880126ff877e52da09d70e805b9c22
William G <williamjesusgarciasanchez2004@gmail.com>
2025-12-06 20:28:21 -0500

    Add python script to challenge 1

---
+++ challenges/2025/challenge1/script.py
@@ -0,0 +1,11 @@
+def filterGifts (gifts): 
+    return list(filter(lambda x: "#" not in x, gifts))
+
+resultado1 = filterGifts(['car', 'doll#arm', 'ball', '#train'])
+print(resultado1)      # Salida -----> ['car', 'ball']
+
+resultado2 = filterGifts(['#broken', '#rusty'])
+print(resultado2)      # Salida -----> []
+
+resultado3 = filterGifts([])
+print(resultado3)      # Salida -----> []
